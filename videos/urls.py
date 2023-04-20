a3fbe3dada97d031257c1a120845f24a1e577cb9
Marc Baumholz <marcbaumholz@t-online.de>
2023-04-20 08:05:45 +0200

    first commit

---
+++ videos/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.urls import path
+from .views import CreateVideo, DetailVideo
+
+urlpatterns = [
+    path('create/', CreateVideo.as_view(), name='video-create'),
+    path('<int:pk>/', DetailVideo.as_view(), name='video-detail'),
+]
+
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import CreateVideo, DetailVideo, UpdateVideo, DeleteVideo
+
+urlpatterns = [
+    path('create/', CreateVideo.as_view(), name='video-create'),
+    path('<int:pk>/', DetailVideo.as_view(), name='video-detail'),
+    path('<int:pk>/update', UpdateVideo.as_view(), name='video-update'),
+    path('<int:pk>/delete', DeleteVideo.as_view(), name='video-delete'),
+]
+"""
